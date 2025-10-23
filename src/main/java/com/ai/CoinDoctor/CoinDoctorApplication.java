d311982f6f4c7a856826f44c0f8372226afa80fa
gusrbs-0404 <gusrbs7534@gmail.com>
2025-10-23 22:07:10 +0900

    Create Project

---
+++ src/main/java/com/ai/CoinDoctor/CoinDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.ai.CoinDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/CoinDoctor/CoinDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.CoinDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
